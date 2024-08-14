f306b40c5da12f4383831daf1af6c766dbd2e907
Barak Dafna <barakdf@gmail.com>
2024-08-14 15:21:00 +0300

    requirements.txt update

--- src/feature_selection.py
+++
@@ -1,6 +0,0 @@
-from sklearn.feature_selection import SelectKBest, chi2
-
-def select_features(X, y, k=20):
-    selector = SelectKBest(chi2, k=k)
-    X_new = selector.fit_transform(X, y)
-    return X_new
